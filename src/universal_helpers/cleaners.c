1c6307eed94a317e3f840c538f50700596766e7a
Jovane Castro <jovanecastro12@gmail.com>
2026-03-02 15:54:48 -0300

    STYLE: norminette

--- src/universal_helpers/cleaners.c
+++ src/universal_helpers/cleaners.c
@@ -1,25 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleaners.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 15:23:20 by jode-cas          #+#    #+#             */
+/*   Updated: 2026/03/02 15:23:21 by jode-cas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-void clean_ms(t_ms *shell)
+void	clean_ms(t_ms *shell)
 {
-  int i;
-  i = 0;
-  if (shell->envs)
-  {
-    while (shell->envs[i])
-    {
-      free(shell->envs[i]);
-      i++;
-    }
-    free(shell->envs);
-  }
+	int	i;
+
+	i = 0;
+	if (shell->envs)
+	{
+		while (shell->envs[i])
+		{
+			free(shell->envs[i]);
+			i++;
+		}
+		free(shell->envs);
+	}
 }
 
-
-void free_matrix(char **m)
+void	free_matrix(char **m)
 {
-    int i = 0;
-    while (m && m[i])
-      free(m[i++]);
-    free(m);
+	int	i;
+
+	i = 0;
+	while (m && m[i])
+		free(m[i++]);
+	free(m);
 }
--- src/universal_helpers/shell_helpers.c
+++ src/universal_helpers/shell_helpers.c
@@ -1,26 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell_helpers.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 15:15:13 by jode-cas          #+#    #+#             */
+/*   Updated: 2026/03/02 15:15:24 by jode-cas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-static void store_envs(t_ms *shell, char **envs)
+static void	store_envs(t_ms *shell, char **envs)
 {
-  int i;
-  i = 0;
-  while (envs[i])
-    i++;
-  shell->envs = ft_calloc(sizeof(char *), i + 1);
-  if (!shell->envs)
-    return;
-  i = 0;
-  while (envs[i])
-  {
-    shell->envs[i] = ft_strdup(envs[i]);
-    i++;
-  }
+	int	i;
+
+	i = 0;
+	while (envs[i])
+		i++;
+	shell->envs = ft_calloc(sizeof(char *), i + 1);
+	if (!shell->envs)
+		return ;
+	i = 0;
+	while (envs[i])
+	{
+		shell->envs[i] = ft_strdup(envs[i]);
+		i++;
+	}
 }
 
-t_ms *create_shell_instance(char **envs)
+t_ms	*create_shell_instance(char **envs)
 {
-  t_ms *shell = ft_calloc(sizeof(t_ms *), 2);
-  store_envs(shell, envs);
-  shell->last_status = 0;
-  return shell;
+	t_ms	*shell;
+
+	shell = ft_calloc(sizeof(t_ms *), 2);
+	store_envs(shell, envs);
+	shell->last_status = 0;
+	return (shell);
 }
