c7ececd705e0f321b1fbb12d32d5014fa422ed07
Victor <vitorvallete@gmail.com>
2026-03-10 14:35:42 -0300

    FEAT: new function to treat sigint(crtl+C) && sigquit(crtl+\)

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 15:13:46 by jode-cas          #+#    #+#             */
-/*   Updated: 2026/03/07 09:54:39 by jode-cas         ###   ########.fr       */
+/*   Updated: 2026/03/10 14:13:51 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include <signal.h>
 
-void SIGINT_handler()
+void sigint_handler(int sig)
 {
-  rl_on_new_line();
-  rl_replace_line("", 0);
-  rl_redisplay();
+	(void)sig;
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	write(1, "\n", 1); //acrescentado para pular linha porque o readline não faz isso automaticamente
+	rl_redisplay();
+}
+void set_signals(void)
+{
+	struct sigaction sa_int;
+	struct sigaction sa_quit;
+
+	sa_int.sa_handler = &sigint_handler;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, NULL);
+
+	sa_quit.sa_handler = SIG_IGN; // sig_ign ignora o sinal ctrl-'\'
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = 0;
+	sigaction(SIGQUIT, &sa_quit, NULL);
 }
